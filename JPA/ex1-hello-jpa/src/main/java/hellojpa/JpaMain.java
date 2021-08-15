3a6e110d88b155a190450370467b0cf99c2d8b46
alanhakhyeonsong <songs4805@naver.com>
2021-08-15 19:15:36 +0900

    엔티티 매핑

--- JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,48 +16,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 회원 등록, 비영속상태
             Member member = new Member();
             member.setId(1L);
-            member.setName("HelloA");
+            member.setUsername("A");
+            member.setRoleType(RoleType.USER);
 
-            // 영속상태
             em.persist(member);
-            /* 회원 조회
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());*/
-
-            /* 회원 삭제
-            Member findMember = em.find(Member.class, 1L);
-            em.remove(findMember);*/
-
-            /* 회원 수정
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");*/
-
-            /**
-             * JPQL
-             * - 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
-             * - SQL을 추상화해서 특정 데이터베이스 SQL에 의존 x
-             * - JPQL을 한마디로 정의하면 객체 지향 SQL
-             */
-            /*
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member1 : result) {
-                System.out.println("member1.name = " + member1.getName());
-            }*/
-            
-            // 영속 엔티티의 동일성 보장
-            Member findMember1 = em.find(Member.class, 1L);
-            Member findMember2 = em.find(Member.class, 1L);
-
-            System.out.println("result = " + (findMember1 == findMember2));
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- JPA/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,14 +1,38 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalTime testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String discription;
+
+    public Member() {
+    }
 
     public Long getId() {
         return id;
@@ -18,11 +42,67 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public LocalTime getTestLocalDate() {
+        return testLocalDate;
+    }
+
+    public void setTestLocalDate(LocalTime testLocalDate) {
+        this.testLocalDate = testLocalDate;
+    }
+
+    public LocalDateTime getTestLocalDateTime() {
+        return testLocalDateTime;
+    }
+
+    public void setTestLocalDateTime(LocalDateTime testLocalDateTime) {
+        this.testLocalDateTime = testLocalDateTime;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
     }
 }
